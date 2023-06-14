045e837bec90808d09d19b5ec038c6b2f88afa1b
bayswiss <107617271+bayswiss@users.noreply.github.com>
2023-06-14 23:52:34 +0200

    Add files via upload

---
+++ autogen_PML.py
@@ -0,0 +1,288 @@
+# AUTO-GENERATING LOCALLY-CONFORMAL PERFECTLY MATCHED LAYER 
+#
+# Copyright (C) 2022-2023 Antonio Baiano Svizzero, Undabit
+# www.undabit.com
+
+# This function takes CAD file and PML quantities as inputs, giving as output all the necessary 
+# functions to build the weak form of the Helmholtz equations in complex coordinates such as
+# Lambda tensor, det(J_PML), etc.
+
+
+
+# The Lambda_PML formulas are the implementation of the following papers:
+# Y. Mi, X. Yu          - Isogeometric locally-conformal perfectly matched layer 
+#                         for time-harmonic acoustics, 2021 
+# H. Beriot, A. Modave  - An automatic perfectly matched layer for acoustic finite
+#                         element simulations in convex domains of general shape, 2020
+# O. Ozgun, M. Kuzoglus - Locally-Conformal Perfeclty Matched Layer implementation 
+#                         for finite element mesh truncation, 2006
+
+
+
+
+
+# description of the inputs: 
+
+# def PML_Functions( CAD_name        = name of the CAD file from which the pml has to be extruded. Tested with step files,
+#                    mesh_size_max   = maximum mesh size used by gmsh,
+#                    Num_layers      = number of PML element sublayers built in the extrusion,
+#                    d_pml           = total thickness of the PML layer, 
+#                    PML_surfaces    = vector containing the id of the surfaces on which the extrusion is performed. 
+#                                      if = -1 gmsh will perform the extrusion on all the 2D surfaces 
+# ):
+
+
+
+def PML_Functions(CAD_name, mesh_size_max, Num_layers, d_pml, PML_surfaces=-1):
+
+        import gmsh
+        import sys
+        import os
+        import numpy as np
+        
+        #initizalizing gmsh
+        gmsh.initialize(sys.argv)
+        gmsh.option.setString('Geometry.OCCTargetUnit', 'M')
+        gmsh.option.setNumber("Mesh.MeshSizeMax", mesh_size_max)
+
+        # merge STEP file\
+        mesh_name_prefix = CAD_name.rpartition('.')[0] 
+        path             = os.path.dirname(os.path.abspath(__file__))
+        gmsh.merge(os.path.join(path, CAD_name))
+
+        # set volume physical group, create volume mesh and export it in .msh
+        gmsh.model.addPhysicalGroup(3, [1], 1, "air_int")
+        gmsh.model.geo.synchronize()
+        gmsh.model.mesh.generate(3)
+        gmsh.write(mesh_name_prefix + "INT.msh")
+        gmsh.model.mesh.clear()
+        gmsh.model.occ.synchronize()
+
+        # set PML interface tags if = -1 every 2D surface gets extruded
+        if PML_surfaces != -1:
+                
+                PML_interface_entities = [(2,s) for s in PML_surfaces]
+        else:
+                PML_interface_entities = gmsh.model.getEntities(2)
+
+        # extrude PML layer of thickness d_PML and Num_layers layers
+        gmsh.option.setNumber('Geometry.ExtrudeReturnLateralEntities', 0)
+        e = gmsh.model.geo.extrudeBoundaryLayer(PML_interface_entities, [Num_layers], [d_pml], False)
+
+        # retrieve tags from PML interface (bottom surf), PML subvolumes
+        bottom_surf  = [s[1] for s in PML_interface_entities]
+        pml_entities = [s for s in e if s[0] == 3]
+        pml_volumes  = [s[1] for s in pml_entities]
+
+        # set PML volume and PML interface physical groups, generate the entire 3D mesh
+        # and export it in .msh file
+        gmsh.model.addPhysicalGroup(3,pml_volumes, 2, "PML")
+        gmsh.model.addPhysicalGroup(2, bottom_surf, 3, "pml_int")
+        gmsh.model.geo.synchronize()
+        gmsh.model.mesh.generate(3)
+        gmsh.write(mesh_name_prefix + "TOT.msh")
+       
+
+        # compute and extract the needed 2D quantities using gmsh API: 
+        #        k2, k3 = principal curvatures of the PML interface
+        #        t2, t3 = principal direction of curvature
+        #        n      = normal of the PML interface
+
+        TAGS  = []
+        K2    = []
+        K3    = []
+        T2    = []
+        T3    = []
+        N     = []
+        COORD = []
+
+        for sub_surf in bottom_surf:
+                tags, coord, param = gmsh.model.mesh.getNodes(2, sub_surf, True)
+                k_2, k_3, t_2, t_3 = gmsh.model.getPrincipalCurvatures(sub_surf,param)
+                n_1 = gmsh.model.getNormal(sub_surf,param)
+
+                COORD.append(coord)     # GMSH doesn't allow to extract the coordinate and
+                K2.append(np.abs(k_2))  # parametrization in one step for all the mesh.
+                K3.append(np.abs(k_3))  # The extraction is performed on the single 
+                T2.append(t_2)          # sub-surface and then all the quantities are
+                T3.append(t_3)          # appended in one vector.
+                TAGS.append(tags)       
+                N.append(n_1)
+
+        COORD2 = np.concatenate(COORD)  # With concatenate we have one single vector of 
+        K22    = np.concatenate(K2)     # quantities for the entire mesh
+        K33    = np.concatenate(K3)     # WARNING! 
+        T22    = np.concatenate(T2)     # Since we retrieved the quantities for every
+        T33    = np.concatenate(T3)     # single sub-surface, we now have duplicates 
+        NN     = np.concatenate(N)      # on the interface between the subsurfaces.
+        TAGSS  = np.concatenate(TAGS)   # np.unique() will solve this
+
+        # delete the duplicate nodes and related quantities retrieve sorted node tags
+        TAGSS_NODUP, indexes = np.unique(TAGSS, return_index=True) 
+
+        # remove duplicates and sort by node tag
+        XX = COORD2[3*indexes]  
+        YY = COORD2[3*indexes+1]
+        ZZ = COORD2[3*indexes+2]
+
+        K22_NODUP = K22[indexes]
+        K33_NODUP = K33[indexes]
+
+        T22_x = T22[3*indexes]  
+        T22_y = T22[3*indexes+1]
+        T22_z = T22[3*indexes+2]
+
+        T33_x = T33[3*indexes]  
+        T33_y = T33[3*indexes+1]
+        T33_z = T33[3*indexes+2]
+
+        N_x = NN[3*indexes]  
+        N_y = NN[3*indexes+1]
+        N_z = NN[3*indexes+2]
+
+
+        # extract 3D nodes coordinates and tags
+        tags3, coord3, param3 = gmsh.model.mesh.getNodes(3, -1, includeBoundary = True, returnParametricCoord = True)
+        tags3_nodup, indexes3 = np.unique(tags3, return_index=True)
+        tags3_PML, coord3_PML = gmsh.model.mesh.getNodesForPhysicalGroup(3,2)
+        
+        # gain for 3D remove duplicates and sort
+        xx=coord3[3*indexes3]  
+        yy=coord3[3*indexes3+1]
+        zz=coord3[3*indexes3+2]
+        gmsh.finalize()
+
+        # start working in FEniCSx: import mesh, define FunctionSpaces, 
+        #                           retrieve dolfinx reordered indexes.
+        import numpy as np
+        from dolfinx.fem import Function, FunctionSpace, Constant, VectorFunctionSpace, TensorFunctionSpace
+        from dolfinx.io import XDMFFile, gmshio
+        from ufl import as_matrix
+        from mpi4py import MPI
+        from petsc4py import PETSc
+        
+        msh, cell_tags, facet_tags  = gmshio.read_from_msh(mesh_name_prefix + "TOT.msh", MPI.COMM_SELF, 0, gdim=3)
+        V                           = FunctionSpace(msh, ("CG",1))
+        VV                          = VectorFunctionSpace(msh, ("CG", 1))
+         
+        indexes_dolfinx             = msh.geometry.input_global_indices
+ 
+        omega                       = Constant(V, PETSc.ScalarType(1))
+        k0                          = Constant(V, PETSc.ScalarType(1))
+        
+        # initialize variables
+        k2_PML                      = np.zeros([len(tags3_nodup)])
+        k3_PML                      = np.zeros([len(tags3_nodup)])
+        d                           = np.zeros([len(tags3_nodup)])
+        t2_x_PML                    = np.zeros([3*len(tags3_nodup)])
+        t2_y_PML                    = np.zeros([3*len(tags3_nodup)])
+        t2_z_PML                    = np.zeros([3*len(tags3_nodup)])
+        t3_x_PML                    = np.zeros([3*len(tags3_nodup)])
+        t3_y_PML                    = np.zeros([3*len(tags3_nodup)])
+        t3_z_PML                    = np.zeros([3*len(tags3_nodup)])
+        n_x_PML                     = np.zeros([3*len(tags3_nodup)])
+        n_y_PML                     = np.zeros([3*len(tags3_nodup)])
+        n_z_PML                     = np.zeros([3*len(tags3_nodup)])
+
+        # Assign the 2D quantities computed on the PML interface to the nodes in the
+        # PML volume. Every node of the PML volume, gets assigned the quantities in
+        # the nearest PML interface node.
+        for n in range(len(tags3_nodup)):
+                if n in tags3_PML:
+                        d_square    = np.sqrt((xx[n] - XX)**2 + (yy[n] - YY)**2 + (zz[n] - ZZ)**2)
+                        min_d       = np.min(d_square)
+                        min_idx     = np.argmin(d_square)
+
+                        k2_PML[n]   = K22_NODUP[min_idx]
+                        k3_PML[n]   = K33_NODUP[min_idx]
+
+                        d[n]        = min_d
+                        
+                        t2_x_PML[n] = T22_x[min_idx]
+                        t2_y_PML[n] = T22_y[min_idx]
+                        t2_z_PML[n] = T22_z[min_idx]
+
+                        t3_x_PML[n] = T33_x[min_idx]
+                        t3_y_PML[n] = T33_y[min_idx]
+                        t3_z_PML[n] = T33_z[min_idx]
+
+                        n_x_PML[n]  = N_x[min_idx]
+                        n_y_PML[n]  = N_y[min_idx]
+                        n_z_PML[n]  = N_z[min_idx]
+
+        # assign the gmsh quantities to the dolfinx functions:
+        k2                    = Function(V)
+        k2.x.array[:]         = k2_PML[indexes_dolfinx]  
+
+
+        k3                    = Function(V)
+        k3.x.array[:]         = k3_PML[indexes_dolfinx]  
+
+
+        phi_domain            = Function(V)
+        phi_domain.x.array[:] = d[indexes_dolfinx]/d_pml
+
+        t2                    = Function(VV)
+        t2_PML                = []
+        for a, b, c in zip(t2_x_PML[indexes_dolfinx], t2_y_PML[indexes_dolfinx], t2_z_PML[indexes_dolfinx]):
+                t2_PML.append([a, b, c])
+        t2.x.array[:]         =  np.concatenate(t2_PML)
+
+        t3                    = Function(VV)
+        t3_PML                = []
+        for a, b, c in zip(t3_x_PML[indexes_dolfinx], t3_y_PML[indexes_dolfinx], t3_z_PML[indexes_dolfinx]):
+                t3_PML.append([a, b, c])
+        t3.x.array[:]         =  np.concatenate(t3_PML)
+
+        n                     = Function(VV)
+        n_PML                 = []
+        for a, b, c in zip(n_x_PML[indexes_dolfinx], n_y_PML[indexes_dolfinx], n_z_PML[indexes_dolfinx]):
+                n_PML.append([a, b, c])
+        n.x.array[:]          =  np.concatenate(n_PML)
+
+
+        # Building the model. Definition of all the functions needed to compute
+        # the stretching functions 
+
+        C = 15*k0 #constant of the sigma function
+
+        # polynomial absorbtion function f(csi) (other functions can be chosen)
+        N_stretching = 3 # degree of the function
+        sigma        = C*phi_domain**N_stretching
+        f_csi        = d_pml*C/(N_stretching+1)*phi_domain**(N_stretching+1)
+
+        # normal parametric coordinate
+        csi   = phi_domain*d_pml 
+
+        # scale factors
+        h2    = 1 + k2*csi      
+        h3    = 1 + k3*csi
+
+        # stretching functions
+        s1    = 1 + 1/(1j*k0)*sigma
+        s2    = 1 + k2 * f_csi/(1j*k0*h2) 
+        s3    = 1 + k3 * f_csi/(1j*k0*h3) 
+
+        # diadic tensors (outer product of the principal directions)
+        nnT   = as_matrix(np.outer(n,n))
+        t2t2T = as_matrix(np.outer(t2,t2))
+        t3t3T = as_matrix(np.outer(t3,t3))
+
+        # determinant of J_PML
+        detJ  = s1*s2*s3
+
+        # PML tensor 
+        LAMBDA_PML = s2*s3/s1 * nnT + s1*s3/s2 * t2t2T + s1*s2/s3 * t3t3T
+
+
+        # Outputs:
+
+        # LAMBDA_PML = PML tensor, to be used in the weak form 
+        # detJ       = determinant of J_PML, to be used in the weak form
+        # omega      = angular frequency
+        # k0         = acoustic wavenumber
+        # msh        = dolfinx msh 
+        # cell_tags  = dolfinx cell tags
+        # facet_tags = dolfinx facet tags
+
+        return LAMBDA_PML, detJ, omega, k0, msh, cell_tags, facet_tags
---
+++ helmholtz_with_PML.py
@@ -0,0 +1,183 @@
+import numpy as np
+import ufl
+from dolfinx import geometry
+from dolfinx.fem import Function, FunctionSpace, assemble_scalar, dirichletbc, locate_dofs_topological, form, Constant, VectorFunctionSpace, TensorFunctionSpace ,Expression
+from dolfinx.fem.petsc import LinearProblem
+from dolfinx.io import XDMFFile, gmshio
+from dolfinx.mesh import create_unit_square, meshtags
+from ufl import dx, grad, inner, outer, dot, ds, Measure, as_matrix, as_tensor, inv, det, log
+from mpi4py import MPI
+from petsc4py import PETSc
+import time
+from autogen_PML import PML_Functions
+from ufl.algorithms.compute_form_data import estimate_total_polynomial_degree
+
+# The code will be executed in frequency-multiprocessing mode:
+# the entire domain will be assigned to one process for every frequency.
+# Set the number of processes to use in parallel:
+n_processes = 4
+
+#frequency range definition
+f_axis = np.arange(50, 3005, 50)
+
+#Mic position definition
+mic = np.array([0.1, 0.1, 0.1]) 
+
+# approximation space polynomial degree of the solution
+deg = 1
+
+# cad name
+CAD_name = 'air.step'
+
+# mesh name 
+mesh_name_prefix =  CAD_name.rpartition('.')[0] 
+
+# PML 
+PML_FLAG      = 1       # 1 if PML_Functions needs to be used, 0 if not
+Num_layers    = 8       # number of PML elements layers
+d_PML         = 0.08   # total thickness of the PML layer
+mesh_size_max = 0.005   # the mesh will be created entirely in gmsh. this sets its maximum
+                        # size
+# PML_surfaces = [4,6]  # vector of tags, identifying the surfaces from which the PML
+                        # layer gets automatically extruded. Comment this line to extrude
+                        # all the 2D surfaces of the system
+
+# PML check
+if PML_FLAG == 1:
+    LAMBDA_PML, detJ, omega, k0, msh, cell_tags, facet_tags = PML_Functions(CAD_name, mesh_size_max, Num_layers, d_PML)
+else:
+    msh, cell_tags, facet_tags = gmshio.read_from_msh(mesh_name_prefix + "TOT.msh", MPI.COMM_SELF, 0, gdim=3)
+    V = FunctionSpace(msh, ("CG", deg))
+    VV = VectorFunctionSpace(msh, ("CG", deg))
+    omega = Constant(V, PETSc.ScalarType(1))
+    k0 = Constant(V, PETSc.ScalarType(1))
+
+# fluid quantities definition
+c0 = 340
+rho_0 = 1.225
+
+# Source amplitude
+Q = 0.0001
+
+#Source definition position = (Sx,Sy)
+Sx = 0.05
+Sy = 0.02
+Sz = 0.02
+
+# Test and trial function space
+V = FunctionSpace(msh, ("CG", deg))
+VVV = TensorFunctionSpace(msh, ("CG", deg))
+
+u = ufl.TrialFunction(V)
+v = ufl.TestFunction(V)  
+f = Function(V)
+
+#Narrow normalized gauss distribution (quasi-monopole)
+alfa          = c0/max(f_axis)/10.1
+delta_tmp     = Function(V)
+delta_tmp.interpolate(lambda x : 1/(np.abs(alfa)*np.sqrt(np.pi))*np.exp(-(((x[0]-Sx)**2+(x[1]-Sy)**2+(x[2]-Sz)**2)/(alfa**2))))
+int_delta_tmp = assemble_scalar(form(delta_tmp*dx)) 
+delta         = delta_tmp/int_delta_tmp
+int_delta     = assemble_scalar(form(delta*dx))
+
+dx = Measure("dx", domain=msh, subdomain_data=cell_tags, metadata={"quadrature_degree": 4})
+
+# weak form definition: 
+f  = 1j*rho_0*omega*Q*delta
+
+a  = inner(grad(u), grad(v)) * dx(1) - k0**2 * inner(u, v) * dx(1)  \
+        + inner(LAMBDA_PML*grad(u), grad(v)) * dx(2) - detJ * k0**2 * inner(u, v) * dx(2)
+
+L  = inner(f, v) * dx(1)
+
+#building the problem
+uh      = Function(V)
+uh.name = "u"
+problem = LinearProblem(a, L, u=uh, petsc_options={"ksp_type": "preonly", "pc_type": "lu","pc_factor_mat_solver_type": "mumps"})
+
+# creation of the mesh without the PML on which the soultion is projected
+msh_INT, cell_tags_INT, facet_tags_INT = gmshio.read_from_msh(mesh_name_prefix + "INT.msh", MPI.COMM_SELF, 0, gdim=3)
+V_INT = FunctionSpace(msh_INT, ("CG", deg))
+uh_NOPML = Function(V_INT)
+
+# spectrum initialization
+p_mic = np.zeros((len(f_axis),1),dtype=complex)
+
+# frequency loop
+print(" ")
+
+start_time = time.time()
+def frequency_loop(nf):
+
+    freq = f_axis[nf]
+    print("Computing frequency: " + str(freq) + " Hz...")  
+
+    # Compute solution
+    omega.value = f_axis[nf]*2*np.pi
+    k0.value    = 2*np.pi*f_axis[nf]/c0
+    problem.solve()
+
+    uh_NOPML.interpolate(uh)
+    
+    #Export field for multiple of 100 Hz frequencies
+    if freq%100 == 0:
+        with XDMFFile(msh.comm,CAD_name + "Solution_" + str(freq) + "Hz.xdmf", "w") as xdmf:
+             xdmf.write_mesh(msh)
+             xdmf.write_function(uh)
+        with XDMFFile(msh_INT.comm,CAD_name + "Solution_" + str(freq) + "Hz_NOPML.xdmf", "w") as xdmf:
+             xdmf.write_mesh(msh_INT)
+             xdmf.write_function(uh_NOPML)
+    
+    # Microphone pressure at specified point evaluation
+    points = np.zeros((3,1))
+    points[0][0] = mic[0]
+    points[1][0] = mic[1]
+    points[2][0] = mic[2]
+    bb_tree = geometry.BoundingBoxTree(msh, msh.topology.dim)
+    cells = []
+    points_on_proc = []
+    # Find cells whose bounding-box collide with the the points
+    cell_candidates = geometry.compute_collisions(bb_tree, points.T)
+    # Choose one of the cells that contains the point
+    colliding_cells = geometry.compute_colliding_cells(msh, cell_candidates, points.T)
+    for i, point in enumerate(points.T):
+        if len(colliding_cells.links(i))>0:
+            points_on_proc.append(point)
+            cells.append(colliding_cells.links(i)[0])
+    points_on_proc = np.array(points_on_proc, dtype=np.float64)
+    u_values = uh.eval(points_on_proc, cells)
+    
+    #building of sound pressure vector
+    return u_values
+
+# MultiProcessing: every frequency the entire domain gets assigned to on one process
+from multiprocessing import Pool
+nf = range(0,len(f_axis))
+if __name__ == '__main__':
+    print("Computing...")  
+    pool = Pool(n_processes)                        # Create a multiprocessing Pool
+    p_mic = pool.map(frequency_loop, nf)  # process data_inputs iterable with pool
+    pool.close()
+    pool.join()
+
+
+print ("\033[A                             \033[A")
+end_time = time.time()
+print("JOB COMPLETED in " + str(end_time - start_time)+ " s")
+
+
+#creation of file [frequency pressure]
+Re_p = np.real(p_mic)
+Im_p = np.imag(p_mic)
+arr  = np.column_stack((f_axis,Re_p,Im_p))
+print("Saving data to .csv file...")
+np.savetxt("p_mic.csv", arr, delimiter=",")
+
+#spectrum plot
+import matplotlib.pyplot as plt
+fig = plt.figure()
+plt.plot(f_axis, 20*np.log10(np.abs(p_mic)/2e-5), "k", linewidth=1, label="Sound pressure [dB]")
+plt.grid(True)
+plt.xlabel("frequency [Hz]")
+plt.legend()
+plt.show()
